38486cbf934a9459be57cb68d848ed794bead806
akhil111 <akhil.maddu021@gmail.com>
2022-09-28 15:29:29 +0530

    var changes

--- dev-env/variables.tf
+++ dev-env/variables.tf
@@ -1,27 +1,27 @@
 variable "project_id" {
   type        = string
   description = "name of the project id"
-
+  default     = "aem-dev-363905"
 }
 
 
 variable "firewall_name" {
   type        = string
   description = "name of the project id"
+  default     = "dev-aem-firewall"
 }
 
 variable "firewall_name2" {
   type        = string
   description = "name of the project id"
+  default     = "dev-aem-firewall-dev"
 }
 
 variable "network" {
   type        = string
   description = "name of the project id"
+  default     = "default"
 }
 
-variable "bucket" {
-  type        = string
-  description = "name of the project id"
-}
+
 
